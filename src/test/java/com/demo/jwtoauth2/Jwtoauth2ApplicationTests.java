f42c753e921e33b88e634e6e1fabf3cc4451c6b7
limbuu <keshab@fusemachines.com>
2018-04-16 14:01:10 +0545

    jwt implemetation with oauth2 for spring boot apis

---
+++ src/test/java/com/demo/jwtoauth2/Jwtoauth2ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.demo.jwtoauth2;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Jwtoauth2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
